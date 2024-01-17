0bad62a5faa92c66ca2aa80d52f2902c4f7e11cc
extdperperm <ext.dperperm@gmail.com>
2024-01-17 09:00:15 +0000

    Inicio de proyecto

---
+++ src/test/java/es/dsw/main/MiProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dsw.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
